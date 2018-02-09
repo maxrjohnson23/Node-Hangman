f21cfb0112bc298c834f79117453458b808a360c
Max Johnson <maxrjohnson23@gmail.com>
2018-02-09 10:36:59 -0600

    Add colorized output using chalk.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Word = require('./word');
-// Create as singleton
 const WordBank = require('./wordbank')();
 const inquirer = require('inquirer');
+const chalk = require('chalk');
 
 // Prompt for user input
 let question = [{
@@ -27,25 +27,27 @@ function promptUserGuess(word, userGuesses) {
         word.guessLetter(userLetter);
 
         let displayWord = word.displayWord();
-        console.log(displayWord);
+        console.log(chalk.bold(`\n ${displayWord} \n`));
 
         // Evaluate user's guess
         if(displayWord.includes(userLetter)) {
-            console.log('Correct!');
+            console.log(chalk.green('Correct!'));
         } else {
-            console.log('Incorrect!');
+            console.log(chalk.red('Incorrect!'));
             userGuesses--;
         }
+        // Display user's remaining guesses
+        console.log(`\n${chalk.red("Guesses remaining:")} ${chalk.red.bold(userGuesses)}`);
 
         // Word completed, finish the game
         if(!displayWord.includes('_')) {
-            console.log('You Win!!');
+            console.log(chalk.bold.green('\n You Win!! \n'));
             // See if the user wants to play again
             playAgain();
         } else if (userGuesses === 0) {
-            console.log('You Lose..');
+            console.log(chalk.bold.red('\n You Lose.. \n'));
             // Reveal the word
-            console.log(`The word was: ${word.peekWord()}`);
+            console.log(`The word was: ${word.peekWord()} \n`);
             // See if the user wants to play again
             playAgain();
         } else {
@@ -60,7 +62,7 @@ function playAgain() {
         if(answer.playAgain.toUpperCase() === "Y") {
             startGame();
         } else {
-            console.log('Thanks for playing!');
+            console.log(chalk.bold.blue('Thanks for playing!'));
         }
     })
 }
@@ -68,15 +70,20 @@ function playAgain() {
 
 function startGame() {
     // Initialize user guesses
-    let remainingGuesses = 2;
+    let remainingGuesses = 9;
 
     // Generate random word
     const generatedWord = WordBank.generateWord().toUpperCase();
+    // **HIDDEN** console.log(generatedWord);
     const word = new Word(generatedWord);
-    console.log(generatedWord);
-    console.log(word.displayWord());
+
+    console.log(chalk.bold(`${word.displayWord()} \n`));
+
     promptUserGuess(word, remainingGuesses);
 }
 
+console.log(chalk.bold.blue('----------------------------------'));
+console.log(chalk.bold.green('Welcome to Hangman - Cars Edition!'));
+console.log(chalk.bold.blue('---------------------------------- \n'));
 startGame();
 
