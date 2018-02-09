b0715bd1fa5f6197a9a5613da8a78c13941bf1a3
Max Johnson <maxrjohnson23@gmail.com>
2018-02-09 09:28:52 -0600

    Add play again functionality

--- index.js
+++ index.js
@@ -3,29 +3,50 @@ const Word = require('./word');
 const WordBank = require('./wordbank')();
 const inquirer = require('inquirer');
 
-// Generate random word
-const generatedWord = WordBank.generateWord().toUpperCase();
-const word = new Word(generatedWord);
-
-console.log(generatedWord);
-
 let question = [{
     name: 'userGuess',
     message: 'Guess a letter'
 }];
 
-function promptUser() {
+let again = [{
+    name: 'playAgain',
+    message: "Would you like to play again?"
+}];
+
+function promptUser(word) {
     inquirer.prompt(question).then(answer => {
-        word.guessLetter(answer.userGuess);
+        word.guessLetter(answer.userGuess.toUpperCase());
         let displayWord = word.displayWord();
         console.log(displayWord);
         if(displayWord.includes('_')) {
-            promptUser();
+            promptUser(word);
         } else {
             console.log('Win!');
+            playAgain();
         }
     });
 };
 
-promptUser();
+function playAgain() {
+    inquirer.prompt(again).then(answer => {
+        if(answer.playAgain) {
+            startGame();
+        } else {
+            console.log('Bye!');
+        }
+    })
+}
+
+
+
+function startGame() {
+    // Generate random word
+    const generatedWord = WordBank.generateWord().toUpperCase();
+    const word = new Word(generatedWord);
+    console.log(generatedWord);
+    console.log(word.displayWord());
+    promptUser(word);
+}
+
+startGame();
 
